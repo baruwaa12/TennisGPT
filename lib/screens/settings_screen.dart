804f041e33f323b52b2ee4f3656d9588082c402c
baruwaa12 <baruwaademola@outlook.com>
2026-01-02 08:44:23 +0000

    Build 16

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -196,7 +196,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
               child: GestureDetector(
                 onTap: _handleVersionTap,
                 child: Text(
-                  'TennisGPT v1.0.0 (Build 14)',
+                  'TennisGPT v1.0.0 (Build 16)',
                   style: GoogleFonts.poppins(
                     fontSize: 12,
                     color: Colors.grey[500],
