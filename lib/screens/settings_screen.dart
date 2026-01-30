588393d3b74aa00d7287f17285c6d318685e5fca
baruwaa12 <baruwaademola@outlook.com>
2026-01-30 16:59:09 +0000

    fixed build number

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -196,7 +196,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
               child: GestureDetector(
                 onTap: _handleVersionTap,
                 child: Text(
-                  'Composure v1.0.0 (Build 36)',
+                  'Composure v1.0.0 (Build 37)',
                   style: GoogleFonts.poppins(
                     fontSize: 12,
                     color: Colors.grey[500],
