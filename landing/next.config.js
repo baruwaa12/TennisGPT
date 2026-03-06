b79cf86ee73771f4c564b1373759b931bdef00af
baruwaa12 <baruwaademola@outlook.com>
2026-03-06 17:09:15 +0700

    fix redirect loop - removed unnecessary rewrites from next.config

--- landing/next.config.js
+++ landing/next.config.js
@@ -3,25 +3,6 @@ const nextConfig = {
   images: {
     unoptimized: false,
   },
-  async rewrites() {
-    return {
-      afterFiles: [
-        {
-          source: "/app/:path((?!.*\\.).*)",
-          destination: "/app/index.html",
-        },
-      ],
-    };
-  },
-  async redirects() {
-    return [
-      {
-        source: "/app",
-        destination: "/app/",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
