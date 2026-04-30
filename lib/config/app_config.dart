d0315aee4452e5f188a38385e7f7ae2bb3211928
baruwaa12 <baruwaademola@outlook.com>
2026-04-30 15:13:17 +0700

    build number 59

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -66,7 +66,7 @@ class AppConfig {
   
   static const String appName = 'Composure';
   static const String appVersion = '1.0.0';
-  static const String buildNumber = '58';
+  static const String buildNumber = '59';
   static const String supportEmail = 'ademolabaruwa09@gmail.com';
   static const String termsUrl = 'https://composure.app/terms';
   static const String privacyUrl = 'https://composure.app/privacy';
